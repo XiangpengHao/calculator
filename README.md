1ca9010e56edc5419afae248521e2110b5a6ac79
Xiangpeng Hao <me@haoxp.xyz>
2024-04-22 12:54:54 -0500

    update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# A simple number converter 
+
+Mostly written by ChatGPT.
+
 # create-svelte
 
 Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/main/packages/create-svelte).
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	let message = writable('');
 
 	let decimal = '';
 	let octal = '';
+	let octal_pretty = '';
 	let binary = '';
+	let binary_pretty = '';
 	let hexadecimal = '';
+	let hexadecimal_pretty = '';
 	let bits: string[] = Array(64).fill('0');
 
 	function updateAllFromBits() {
@@ -15,8 +21,12 @@
 	function updateAllFromNumber(num: bigint) {
 		decimal = num.toString(10);
 		octal = num.toString(8);
+		octal_pretty = '0o' + octal.match(/.{1,3}/g)!.join('_');
+
 		binary = num.toString(2).padStart(64, '0');
+		binary_pretty = '0b' + binary.match(/.{1,8}/g)!.join('_');
 		hexadecimal = num.toString(16).toUpperCase().padStart(16, '0');
+		hexadecimal_pretty = '0x' + hexadecimal.match(/.{1,4}/g)!.join('_');
 		updateBitsFromNumber(num);
 	}
 
@@ -37,6 +47,24 @@
 	}
 	function convertFromHexadecimal(value: string) {
 		updateFromInput(value, 16);
+	}
+
+	async function copyToClipboard(text: string) {
+		try {
+			await navigator.clipboard.writeText(text);
+			message.set('Copied to Clipboard!');
+			setTimeout(() => message.set(''), 2000); // Clear message after 2 seconds
+		} catch (err) {
+			message.set('Failed to copy!');
+			setTimeout(() => message.set(''), 2000);
+		}
+	}
+
+	function handleKeyPress(event: any, text: string) {
+		if (event.key === 'Enter' || event.key === ' ') {
+			copyToClipboard(text);
+			event.preventDefault(); // Prevent the default action for these keys
+		}
 	}
 
 	function updateFromInput(value: string, base: number) {
@@ -78,58 +106,81 @@
 </script>
 
 <div style="margin-left: 1em;">
-	<h1>Calculator</h1>
+	<h1>Bit Calculator</h1>
 	<div class="input-box">
 		<label for="decimal">Decimal:</label>
-		<input
-			id="decimal"
-			type="text"
-			bind:value={decimal}
-			on:input={() => convertFromDecimal(decimal)}
-			placeholder="Decimal"
-		/>
+		<div>
+			<input
+				id="decimal"
+				type="text"
+				bind:value={decimal}
+				on:input={() => convertFromDecimal(decimal)}
+				placeholder="Decimal"
+			/>
+		</div>
 	</div>
 
 	<div class="input-box">
 		<label for="octal">Octal:</label>
-		<input
-			id="octal"
-			type="text"
-			bind:value={octal}
-			on:input={() => convertFromOctal(octal)}
-			placeholder="Octal"
-		/>
+		<div>
+			<input
+				id="octal"
+				type="text"
+				bind:value={octal}
+				on:input={() => convertFromOctal(octal)}
+				placeholder="Octal"
+			/>
+		</div>
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div class="alternative-format" on:click={() => copyToClipboard(octal_pretty)}>
+			{octal_pretty}
+		</div>
 	</div>
 
 	<div class="input-box">
-		<label for="binary">Binary:</label>
-		<input
-			id="binary"
-			type="text"
-			bind:value={binary}
-			on:input={() => convertFromBinary(binary)}
-			placeholder="Binary"
-		/>
+		<label for="binary">Bin:</label>
+		<div>
+			<input
+				id="binary"
+				type="text"
+				bind:value={binary}
+				on:input={() => convertFromBinary(binary)}
+				placeholder="Bin"
+			/>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<div class="alternative-format" on:click={() => copyToClipboard(binary_pretty)}>
+				{binary_pretty}
+			</div>
+		</div>
 	</div>
 	<div class="input-box">
-		<label for="hexadecimal">Hexadecimal:</label>
-		<input
-			id="hexadecimal"
-			type="text"
-			bind:value={hexadecimal}
-			on:input={() => convertFromHexadecimal(hexadecimal)}
-			placeholder="Hexadecimal"
-		/>
+		<label for="hexadecimal">Hex:</label>
+		<div class="values">
+			<input
+				id="hexadecimal"
+				type="text"
+				bind:value={hexadecimal}
+				on:input={() => convertFromHexadecimal(hexadecimal)}
+				placeholder="Hex"
+			/>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<div class="alternative-format" on:click={() => copyToClipboard(hexadecimal_pretty)}>
+				{hexadecimal_pretty}
+			</div>
+		</div>
 	</div>
 
 	<div class="input-box">
-		<div>Bit Values:</div>
+		<div>Bit bits:</div>
 		<div>
 			<div class="bit-container">
 				{#each Array.from({ length: 8 }, (_, i) => 7 - i) as i}
 					<div class="bit-group">
 						{#each [...bits.slice(i * 8, i * 8 + 8)].reverse() as bit, index}
-							<div>
+							<div class="one-bit">
 								<div>
 									<button
 										type="button"
@@ -162,10 +213,23 @@
 	}
 
 	input[type='text'] {
-		width: 95%; /* Adjust width to fill container */
+		width: 95%;
 		margin: 10px 2.5%; /* Center and provide vertical space */
+		margin-bottom: 0;
+		margin-top: 0;
 		padding: 8px;
 		font-size: 16px; /* Increase font size for readability */
+	}
+	.alternative-format {
+		margin: 10px 2.5%;
+		margin-bottom: 0;
+		margin-top: 0;
+		padding: 8px;
+		color: rgb(119, 135, 135);
+	}
+
+	.alternative-format:hover {
+		cursor: pointer;
 	}
 
 	button.bit-box {
@@ -192,6 +256,10 @@
 		margin-bottom: 1em;
 	}
 
+	/* .values {
+		margin-bottom: 1em;
+	} */
+
 	.bit-container {
 		display: flex;
 		flex-wrap: wrap;
@@ -204,12 +272,13 @@
 		margin-left: 2em;
 		margin-bottom: 1em;
 	}
+
+	.bit-group .one-bit:nth-child(5) {
+		margin-left: 0.5em;
+	}
+
 	.bit-index {
 		font-size: small;
 		color: #a0a0a0;
 	}
-
-	/* .bit-container div:nth-child(8n) {
-		margin-right: 1em;
-	} */
 </style>
